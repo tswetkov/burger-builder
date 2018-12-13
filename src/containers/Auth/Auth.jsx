b07fca6eb8586f15b84994e44d796a8a38a37298
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-12-13 23:45:22 +1000

    Начал делать авторизацию

---
+++ src/containers/Auth/Auth.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Auth extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          {/*  */}
+          {/*  */}
+        </form>
+      </div>
+    );
+  }
+}
+export default Auth;
