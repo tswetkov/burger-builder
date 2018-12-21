88e3f72281e0cdc74781a8ca2a49f0186e15af8b
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-12-22 00:58:49 +1000

    Закончил модуль авторизацииgit st!

--- src/containers/Checkcout/ContactData/ContactData.jsx
+++ src/containers/Checkcout/ContactData/ContactData.jsx
@@ -115,7 +115,8 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     };
 
     this.props.onOrderBurger(order, this.props.token);
@@ -214,7 +215,8 @@ const mapState = state => {
     ingredients: state.ingredients.ingredients,
     price: state.ingredients.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 };
 
--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -30,6 +30,9 @@ export const authFailure = error => {
 };
 
 export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("expirationDate");
+  localStorage.removeItem("userId");
   return {
     type: AUTH_LOGOUT
   };
@@ -61,6 +64,12 @@ export const auth = (email, password, isSignup) => {
         authData
       )
       .then(response => {
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem("token", response.data.idToken);
+        localStorage.setItem("expirationDate", expirationDate);
+        localStorage.setItem("userId", response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -74,5 +83,27 @@ export const setAuthRedirectPath = path => {
   return {
     type: SET_AUTH_REDIRECT_PATH,
     path
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem("userId");
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
   };
 };
